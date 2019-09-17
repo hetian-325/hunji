82faf2cbfc399c1ca4ffc8fffa3b4710a036308f
hetian <544205481@qq.com>
2019-09-17 11:48:15 +0800

    测试图片

---
+++ src/redux/reducer.js
@@ -0,0 +1,12 @@
+var obj=[
+    {id:1,title:"婚纱礼服",imgurl:"nav01.png"},
+    {id:2,title:"婚房布置",imgurl:"nav02.png"},
+    {id:3,title:"婚鞋箱包",imgurl:"nav03.png"},
+    {id:4,title:"喜糖回礼",imgurl:"nav04.png"},
+    {id:5,title:"婚礼百货",imgurl:"nav05.png"},
+    {id:6,title:"婚庆床品",imgurl:"nav06.png"},
+    {id:7,title:"睡衣贴胸",imgurl:"nav07.png"},
+    {id:8,title:"良辰吉市",imgurl:"nav08.png"},
+    {id:9,title:"美妆护肤",imgurl:"nav09.png"},
+    {id:10,title:"全部分类",imgurl:"nav10.png"}
+]
